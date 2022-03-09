6aaa18ac6c40ae9783f5761141ac82b4c23206f1
dessale4 <dessale4@gmail.com>
2022-03-09 10:10:35 -0600

    controller endpoint modified for accepting path variable

--- src/main/java/com/intouch/deployToTomcatWithJenkinsCICD/controller/RestApiController.java
+++ src/main/java/com/intouch/deployToTomcatWithJenkinsCICD/controller/RestApiController.java
@@ -1,6 +1,7 @@
 package com.intouch.deployToTomcatWithJenkinsCICD.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class RestApiController {
 	
-	@GetMapping("/getMessage")
-	public String getMessage() {
-		return "Incomming message is =======";
+	@GetMapping("/getMessage/{message}")
+	public String getMessage(@PathVariable String message) {
+		return "Incomming message is ======= " + message;
 	}
 }
